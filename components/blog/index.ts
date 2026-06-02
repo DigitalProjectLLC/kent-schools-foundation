3b0f6ade1f8467dd82e0f2c10d9b002961e0187d
digitalprojectllc <contact@digitalprojectllc.com>
2026-06-02 12:39:51 -0700

    feat: add index.ts blog MDX component

---
+++ components/blog/index.ts
@@ -0,0 +1,5 @@
+export { default as BlogChart } from './BlogChart';
+export { default as StatCallout } from './StatCallout';
+export { default as ComparisonTable } from './ComparisonTable';
+export { default as FAQSchema } from './FAQSchema';
+export { default as Callout } from './Callout';
